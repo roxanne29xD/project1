db278b6e9ccf33d5b67ef36071a27ef777c20371
roxanne29xD <roxannemallavo@yahoo.com>
2013-03-23 13:12:14 +0800

    Update main.cpp

--- project1/main.cpp
+++ project1/main.cpp
@@ -11,21 +11,34 @@ const string PROBLEMS[]={
     "Multiples of 3 and 5",
     "Even Fibonacci",
     "Largest Prime Factor",
-    "Largest Palindrom Product",
+    "Largest Palindrome Product",
     "Smallest Multiple",
     "Sum and Square Difference",
     "The 10001st Prime Number",
     "Largest Product in Series",
     "Special Phythagoran Triplet",
     "Summation of Primes",
+    "Largest Product in a Grid",
+    "Highly Divisible Triangular Number",
+    "Large Sum",
+    "Longest Collatz Sequence",
+    "Diophantine Reciprocals",
+    "Power Digit Sum",
+    "Number Letter Counts",
+    "Maximum Path Sum I",
+    "Counting Sundays",
+    "Factorial Digit Sum",
+    "Name Score",
+    "1000-digit Fibonacci Number",
+    "Truncatable primes",
     "Exit"
 };
 
-const int NUM_PROBLEMS = 10;
+const int NUM_PROBLEMS = 23;
 
 const int EXIT = NUM_PROBLEMS + 1;
 int main (){
-    projecy pro;
+    project pro;
     int ch=0;
 
     bool done=false;
@@ -103,10 +116,89 @@ int main (){
                 system("pause");
                 break;
 
+            case 11:
+
+                pro.largest_product_in_grid();
+                system("pause");
+                break;
+
+            case 12:
+
+                pro.highly_divisible_triangular_number();
+                system("pause");
+                break;
+
+            case 13:
+
+                pro.large_sum();
+                system("pause");
+                break;
+
+            case 14:
+
+                pro.longest_collatz_sequence();
+                system("pause");
+                break;
+
+            case 15:
+
+                pro.diophantine_reciprocals_a();
+                system("pause");
+                break;
+
+            case 16:
+
+                pro.power_digit_sum();
+                system("pause");
+                break;
+
+            case 17:
+
+                pro.number_letter_counts();
+                system("pause");
+                break;
+
+            case 18:
+
+                pro.maximum_path_sum_I();
+                system("pause");
+                break;
+
+            case 19:
+
+                pro.counting_sundays();
+                system("pause");
+                break;
+
+            case 20:
+
+                pro.factorial_digit_sum();
+                system("pause");
+                break;
+
+            case 21:
+
+                pro.name_score();
+                system("pause");
+                break;
+
+            case 22:
+
+                pro.first_1000_digit_fibonacci();
+                system("pause");
+                break;
+
+            case 23:
+
+                pro.truncable_primes();
+                system("pause");
+                break;
+
+
             case EXIT:
-            done=true;
-            break;
+                done=true;
+                break;
         }
-    }
+    }//closing bracket of while
 return 0;
-}
+}// closing bracket of main
